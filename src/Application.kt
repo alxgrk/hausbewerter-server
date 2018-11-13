8e3da7847e0dc7a5b1e6278993faa16b147f0ab9
alxgrk <alexgirke@gmail.com>
2018-11-13 17:06:14 +0100

    cleaning up

--- src/Application.kt
+++ src/Application.kt
@@ -17,6 +17,7 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
     install(ContentNegotiation) {
         jackson {
             enable(SerializationFeature.INDENT_OUTPUT)
@@ -30,35 +31,24 @@ fun Application.module(testing: Boolean = false) {
         filter { call -> call.request.path().startsWith("/") }
     }
 
-    install(ConditionalHeaders)
-
     install(CORS) {
         method(HttpMethod.Options)
         method(HttpMethod.Put)
         method(HttpMethod.Delete)
         method(HttpMethod.Patch)
         header(HttpHeaders.Authorization)
-        header("MyCustomHeader")
         allowCredentials = true
-        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+        host("localhost")
+        host("alxgrk.github.io")
     }
 
-    install(CachingHeaders) {
-        options { outgoingContent ->
-            when (outgoingContent.contentType?.withoutParameters()) {
-                ContentType.Text.CSS -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = 24 * 60 * 60), expires = null as? GMTDate?)
-                else -> null
-            }
-        }
-    }
-
-    install(DefaultHeaders) {
-        header("X-Engine", "Ktor") // will send this header with each response
-    }
+    install(DefaultHeaders) {}
 
     routing {
         get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+            val uri = call.request.uri
+            val headers = call.response.headers
+            call.respondText("Hello World from '$uri' with ${headers.allValues()}!", contentType = ContentType.Text.Plain)
         }
 
         get("/json/jackson") {
